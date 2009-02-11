44f34b673d88e8e2a7161920b8e297069a894a7a
Tamas Kovacs <kovisoft@gmail.com>
2009-02-11 22:25:56 +0100

    Comments

--- plugin/slimv.py
+++ plugin/slimv.py
@@ -4,8 +4,8 @@
 #
 # Client/Server code for Slimv
 # slimv.py:     Client/Server code for slimv.vim plugin
-# Version:      0.1.1
-# Last Change:  04 Feb 2009
+# Version:      0.1.2
+# Last Change:  11 Feb 2009
 # Maintainer:   Tamas Kovacs <kovisoft at gmail dot com>
 # License:      This file is placed in the public domain.
 #               No warranty, express or implied.
